7ba066fc08df6811b61b2da1db61b85ebfc9c540
talbii <ido@tal.bi>
2023-11-30 00:57:19 +0200

    Small nitpicks

--- bench.hpp
+++ bench.hpp
@@ -2,19 +2,44 @@
 #define talbi__BENCH_HPP
 
 /*
- * Set the macro `Parallel` to either std::execution::par_unseq,
- *  (if the compiler supports it), or nothing.  This makes summation
- *  in bench_stat::total() (much) faster. 
+ * bench.hpp: talbi's simple benching utilities.
+ *
+ *  Usage. run `bench(trials, f, Args...)` to test the call f(Args...) for `trials` times.
+ *         Returns a `bench_stat` struct which contains some information about the bench.
+ *         Print a `bench_stat` using the standard operator<<, or access the data directly
+ *         by accessing bench_stat.data, a field of std::vector<double>.
+ *
+ *         It is possible to change the measure time (e.g., measure time in nanoseconds instead
+ *         of microseconds by) changing the value of the template argument Period. In practice,
+ *         it suffices to call bench<std::nano>(.) to measure in nanoseconds instead of the default
+ *         microseconds. bench<Period>(.) should support all of the ratios defined in <ratio>,
+ *         as well as ratio<60> (minute), ratio<3600> (hour) and ratio<86400> (day).
+ *
+ *         One may also access the fields min(), max(), avg() and total(). Once the respective
+ *         functions have been called access is also possible using bench_stat.m_<property>.
+ *
+ *   Note. Compatible both with g++ and clang++. May produce faster code using g++ (see talbi__Parallel).
+ * */
+
+
+/*
+ * Set the macro `talbi__Parallel` to `std::execution::par_unseq,`
+ *  (if the compiler supports it), or nothing. This can make summation
+ *  in bench_stat::total() faster, by potentially parallelising it.
  *
  * More importantly, this makes `bench` perform its benchmarks on
  *  seperate threads, which hopefully will result in faster total
  *  benchmark time.
+ *
+ * Libc++ (clang; LLVM and Apple) have yet to implement <execution>,
+ *  hence prompting this #if. Note that this means that G++ could
+ *  potentially provide a faster binary (this parallelism is mearly a suggestion).
  * */
-#if(defined(__GNUC__) && !defined(__llvm__) && __cplusplus >= 201703L)
+#if (defined(__GNUC__) && !defined(__llvm__) && __cplusplus >= 201703L && !defined(talbi__Parallel))
 #include <execution>
-#define Parallel std::execution::par_unseq, 
+#define talbi__Parallel std::execution::par_unseq, 
 #else
-#define Parallel
+#define talbi__Parallel
 #endif
 
 #include "ratio_to_string.hpp"
@@ -27,25 +52,6 @@
 #include <iostream>   // std::cout (default parameter for print_stat)
 #include <ostream>    // std::basic_ostream
 
-/*
- * bench.hpp: talbi's simple benching utilities.
- *
- *  Usage. run `bench(trials, f, Args...)` to test the call f(Args...) for `trials` times.
- *         Returns a `bench_stat` struct which contains some information about the bench.
- *         Print a `bench_stat` using the standard operator<<, or access the data directly
- *         by accessing bench_stat.data, a field of std::vector<double>.
- *
- *         It is possible to change the measure time (e.g., measure time in nanoseconds instead 
- *         of microseconds by) changing the value of the template argument Period. In practice,
- *         it suffices to call bench<std::nano>(.) to measure in nanoseconds instead of the default
- *         microseconds. bench<Period>(.) should support all of the ratios defined in <ratio>,
- *         as well as ratio<60> (minute), ratio<3600> (hour) and ratio<86400> (day).
- *
- *         One may also access the fields min(), max(), avg() and total(). Once the respective
- *         functions have been called access is also possible using bench_stat.m_<property>.
- *
- *   Note. Compatible both with g++ and clang++. May produce faster code using g++ (parallel for_each, reduce) 
- * */
 
 struct bench_stat {
     using property = std::optional<double>;
@@ -63,25 +69,25 @@ struct bench_stat {
     bench_stat(const std::vector<double>& data, const char *unit) : data(data), trials(data.size()),
                                                                                      unit(unit) {}
 
-    constexpr double min(void) {
+    double min(void) {
         if(m_min) return m_min.value();
         this->get_minmax();
         return m_min.value();
     }
 
-    constexpr double max(void) {
+    double max(void) {
         if(m_max) return m_max.value();
         this->get_minmax();
         return m_max.value();
     }
 
-    constexpr double avg(void) {
+    double avg(void) {
         if(m_avg) return m_avg.value();
         this->get_totalavg();
         return m_avg.value();
     }
 
-    constexpr double total(void) {
+    double total(void) {
         if(m_total) return m_total.value();
         this->get_totalavg();
         return m_total.value();
@@ -95,7 +101,7 @@ private:
     }
 
     void get_totalavg(void) {
-        m_total = std::reduce(Parallel data.cbegin(), data.cend());
+        m_total = std::reduce(talbi__Parallel data.cbegin(), data.cend());
         m_avg = m_total.value() / trials;
     }
 };
@@ -105,8 +111,8 @@ template<class Period = std::milli,
             class F, class... Args>
 bench_stat bench(std::size_t trials, F f, Args... args) {
     std::vector<double> data(trials);
-    std::for_each(Parallel data.begin(), data.end(), [&](auto &i) { 
-#ifdef BENCH_LEGACy
+    std::for_each(talbi__Parallel data.begin(), data.end(), [&](auto &i) { 
+#ifdef BENCH_LEGACY
 #define talbi__clock std::chrono::high_resolution_clock
 #else
 #define talbi__clock std::chrono::steady_clock
@@ -156,5 +162,5 @@ auto& operator<<(std::basic_ostream<Char,Traits> &out, bench_stat &&data) {
     return print_stat(data, out);
 }
 
-#undef Parallel
+#undef talbi__Parallel
 #endif //talbi__BENCH_HPP
